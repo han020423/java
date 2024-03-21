98094481a013a24c624c3c180671976e67886c97
Jaehyuk_han <han020423@gmail.com>
2024-03-21 13:45:55 +0900

    짝수 홀수 판별하는 EvenOdd.java 만듦

---
+++ src/EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("정수를 입력하시오: ");
+		int n = input.nextInt();
+		if(n % 2 == 0)
+			System.out.println("입력된 정수는 짝수입니다.");
+		else
+			System.out.println("입력된 정수는 홀수입니다.");
+
+		System.out.println("프로그램이 종료되었습니다.");
+	}
+
+}
