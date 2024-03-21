e9a37df5f9ce49d044026fd2c32165b06b699f29
Jaehyuk_han <han020423@gmail.com>
2024-03-21 13:59:46 +0900

    정수입력부분 println에서 print로 바꿈

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -4,7 +4,7 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("정수를 입력하시오: ");
+		System.out.print("정수를 입력하시오: ");
 		int n = input.nextInt();
 		if(n % 2 == 0)
 			System.out.println("입력된 정수는 짝수입니다.");
